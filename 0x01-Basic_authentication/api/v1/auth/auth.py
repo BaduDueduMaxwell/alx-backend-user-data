bb809298fd24e451981cec38e6baeb1d99f1d653
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-11-11 19:29:59 +0000

    task 4: Define which routes don't need authentication mandatory

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,9 +7,23 @@ from flask import request
 
 
 class Auth:
+    """
+    Manage API authentication methods
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Determines if authentication is required"""
-        return False  # Placeholder, will implement this logic later
+        if path is None:
+            return True
+        if not excluded_paths:
+            return True
+
+        if not path.endswith('/'):
+            path += '/'
+
+        if excluded_path in excluded_paths:
+            if path == excluded_path:
+                return False
+        return True
 
 
     def authorization_header(self, request=None) -> str:
