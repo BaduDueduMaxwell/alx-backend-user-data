fc2ff56a1b1a37499f57d157241a734c61afd712
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-11-12 14:27:11 +0000

    task 6:Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic Auth class that inherits from Auth.
+    For now, this class is empty, but will be implemented later.
+    """
+    pass
