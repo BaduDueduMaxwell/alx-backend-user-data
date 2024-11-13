be505f6470a6c798d574e0963cc3306d06dc223c
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-11-13 09:03:19 +0000

    task 8:Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Route module for the API
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -21,3 +22,18 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header.split(" ", 1)[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Returns the decoded value of a Base64 string
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded = base64.b64decode(
+                base64_authorization_header, validate=True)
+            return decoded.decode('utf-8')
+        except (base64.binascii.Error, ValueError):
+            return None
