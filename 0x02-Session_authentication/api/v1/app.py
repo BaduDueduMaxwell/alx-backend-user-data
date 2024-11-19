da9389c9dbc0dfbb94d3125df65891c781d121fc
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-11-19 04:21:29 +0000

    task 5:Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -38,7 +38,7 @@ def before_request():
         '/api/v1/auth_session/login/']
     # Do nothing if auth is None or if the path is excluded
     if auth and auth.require_auth(request.path, excluded_paths):
-        if auth.authorization_header(request) is None:
+        if not auth.authorization_header(request) and not auth.session_cookie(request):
             abort(401, description="Unauthorized")
         if auth.current_user(request) is None:
             abort(403, description="Forbidden")
