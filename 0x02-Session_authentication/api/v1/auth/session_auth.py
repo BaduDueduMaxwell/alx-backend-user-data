804661b6b6deb97ab99b5c01b147881e5f32d6ae
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-11-20 02:46:01 +0000

    task 6:Use Session ID for identifying a User

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 SessionAuth module for handling session-based authentication.
 """
 from api.v1.auth.auth import Auth
+from models.user import User
 import uuid
 
 
@@ -35,3 +36,16 @@ class SessionAuth(Auth):
             return None
 
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ Returns a User instance based on a cookie value
+        """
+        session_id = self.session_cookie(request)
+        if not session_id:
+            return None
+
+        user_id = self.user_id_for_session_id(session_id)
+        if not user_id:
+            return None
+
+        return User.get(user_id)
