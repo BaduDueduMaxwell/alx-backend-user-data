186c642cacf354ff638890665ed1b794f250dd2e
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-11-21 15:46:27 +0000

    refactored task 2:Find userr

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -49,11 +49,17 @@ class DB:
 
     def find_user_by(self, **kwargs) -> User:
         """Find a user based on arbitrary keyword arguments"""
-        try:
-            user = self._session.query(User).filter_by(**kwargs).one()
-            return user
-        except NoResultFound:
-            raise NoResultFound(
-                "No user found matching the provided criteria.")
-        except InvalidRequestError:
-            raise InvalidRequestError("Invalid query arguments.")
+        if not kwargs:
+            raise InvalidRequestError("No query parameters provided.")
+
+        column_names = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in column_names:
+                raise InvalidRequestError(f"Invalid query argument: {key}")
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+
+        if user is None:
+            raise NoResultFound("No user found matching the provided criteria.")
+
+        return user
