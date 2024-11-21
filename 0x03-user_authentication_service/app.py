39af431c753d6fa972b087986c364522008934e9
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-11-21 16:09:54 +0000

    task 7:Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -11,5 +11,20 @@ def home():
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route("/users", methods=["POST"])
+def register_user():
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not email or not password:
+        return jsonify({"message": "email and password are required"}), 400
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError as e:
+        return jsonify({"message": str(e)}), 400
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
