097398e1b82478ceacb9db8d0b8384326d2e876a
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-11-21 15:39:22 +0000

    task 2:Find userr

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base
 from user import User
@@ -46,3 +48,14 @@ class DB:
         self._session.commit()
 
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user based on arbitrary keyword arguments"""
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+            return user
+        except NoResultFound:
+            raise NoResultFound(
+                "No user found matching the provided criteria.")
+        except InvalidRequestError:
+            raise InvalidRequestError("Invalid query arguments.")
